1960063a04b4068a420d98d834655f7ae764ac6d
AGilsoul <agilsoulphysics@gmail.com>
2025-05-27 21:20:03 -0600

    new repo

--- project.cmake
+++ project.cmake
@@ -9,3 +9,4 @@ add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Types/")
 
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/TestDeployment/")
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/ProofOfConcept/")
+add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/SolarPanelProof/")
